f028df3efda03e553f2c41424f98e2a39a9e1a22
ioncrisa2 <zyohanes99@gmail.com>
2022-12-31 13:57:45 +0700

    change ip to avd device

--- lib/Helper/dio.dart
+++ lib/Helper/dio.dart
@@ -3,7 +3,7 @@ import 'package:dio/dio.dart';
 Dio dio() {
   return new Dio(
     BaseOptions(
-      baseUrl: 'http://192.168.100.137:8000/api',
+      baseUrl: 'http://10.0.2.2:8000/api',
       connectTimeout: 10000,
       receiveTimeout: 3000,
       headers: {
