ac8483e52df9644c18f6d451840859d9cd3fcfed
ioncrisa2 <zyohanes99@gmail.com>
2022-12-31 13:58:39 +0700

    adding delete data and comment

--- lib/service/job_service.dart
+++ lib/service/job_service.dart
@@ -1,8 +1,11 @@
 import 'package:dio/dio.dart' as Dio;
+import 'package:flutter/foundation.dart';
 import 'package:mobile_client/Helper/dio.dart';
 import 'package:mobile_client/Helper/token_storage.dart';
 import 'package:mobile_client/model/job.dart';
 import 'dart:convert';
+
+import 'package:mobile_client/model/job_detail.dart';
 
 class JobService {
   //ignore: missing_return
@@ -16,7 +19,32 @@ class JobService {
         }));
     if (response.statusCode == 200) {
       final jsonResponse = json.decode(response.toString());
-      return Jobs.fromJson(jsonResponse);
+      final res = Jobs.fromJson(jsonResponse);
+      print(res);
+      return res;
+    }
+  }
+
+  //ignore: missing_return
+  Future<JobDetail> detailJob(id) async {
+    final token = await TokenStorage().readToken();
+    final response = await dio().get(
+      '/jobs/$id',
+      options: Dio.Options(
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json',
+          'Authorization': 'Bearer $token'
+        },
+      ),
+    );
+
+    if (response.statusCode == 200) {
+      final jsonResponse = json.decode(response.toString())['data'];
+
+      final result = JobDetail.fromJson(jsonResponse);
+      debugPrint(result.toString());
+      return result;
     }
   }
 }
