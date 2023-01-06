df6a9346a6ee2d92d3ebea6739b03e5c9553e0e6
ioncrisa2 <zyohanes99@gmail.com>
2023-01-06 21:56:54 +0700

    change api to online

--- lib/Helper/dio.dart
+++ lib/Helper/dio.dart
@@ -3,7 +3,7 @@ import 'package:dio/dio.dart';
 Dio dio() {
   return new Dio(
     BaseOptions(
-      baseUrl: 'http://10.0.2.2:8000/api',
+      baseUrl: 'https://forumandjobserver.info/api',
       connectTimeout: 10000,
       receiveTimeout: 3000,
       headers: {
